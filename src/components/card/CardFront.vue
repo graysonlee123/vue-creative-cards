61312a907646128e47295d08f8cba6c53d0b4871
Grayson Gantek <graysonleegantek@gmail.com>
2020-05-27 16:21:54 -0400

    adds ccTextInput component

--- src/components/card/CardFront.vue
+++ src/components/card/CardFront.vue
@@ -1,25 +1,31 @@
 <template>
   <div class="row">
     <div class="col-sm-6 card edit-area">
-      
+      <cc-text-input></cc-text-input>
     </div>
     <div class="col-sm-6 card card-display"></div>
   </div>
 </template>
 
 <script>
+import TextInput from "./TextInput";
 
+export default {
+  components: {
+    ccTextInput: TextInput
+  }
+}
 </script>
 
 <style>
-  .edit-area {
-    background-color: #d2f9f9;
-    padding: 20px;
-    height: 800px;
-  }
+.edit-area {
+  background-color: #d2f9f9;
+  padding: 20px;
+  height: 800px;
+}
 
-  .card-display {
-    height: 800px;
-    padding: 20px;
-  }
+.card-display {
+  height: 800px;
+  padding: 20px;
+}
 </style>
