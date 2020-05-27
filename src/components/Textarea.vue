606ad80c631ae9fe672110bf720c56eb73a0e55d
Grayson Gantek <graysonleegantek@gmail.com>
2020-05-26 22:16:14 -0400

    adds first global component

---
+++ src/components/Textarea.vue
@@ -0,0 +1,21 @@
+<template>
+  <textarea></textarea>
+</template>
+
+<script>
+new Vue({
+  data: function() {
+    return {
+      textValue: 'Type here'
+    }
+  },
+  methods: {
+    changeText: function() {
+      this.textValue = 'Text changed'
+    }
+  },
+})
+</script>
+
+<style>
+</style>
