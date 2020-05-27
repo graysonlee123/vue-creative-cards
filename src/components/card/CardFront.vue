2a841767af921c42eb91a444daa5e1fcfe35b791
Grayson Gantek <graysonleegantek@gmail.com>
2020-05-27 16:35:56 -0400

    sends input text area data to output text areas with props

--- src/components/card/CardFront.vue
+++ src/components/card/CardFront.vue
@@ -5,12 +5,17 @@
       <cc-text-input @displayTextChanged="textBoxValue2 = $event"></cc-text-input>
       <cc-text-input @displayTextChanged="textBoxValue3 = $event"></cc-text-input>
     </div>
-    <div class="col-sm-6 card card-display"></div>
+    <div class="col-sm-6 card card-display">
+      <cc-text-output :displayText="textBoxValue1"></cc-text-output>
+      <cc-text-output :displayText="textBoxValue2"></cc-text-output>
+      <cc-text-output :displayText="textBoxValue3"></cc-text-output>
+    </div>
   </div>
 </template>
 
 <script>
 import TextInput from "./TextInput";
+import TextOutput from "./TextOutput";
 
 export default {
   data: function() {
@@ -21,7 +26,8 @@ export default {
     }
   },
   components: {
-    ccTextInput: TextInput
+    ccTextInput: TextInput,
+    ccTextOutput: TextOutput
   }
 }
 </script>
---
+++ src/components/card/TextOutput.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <p>{{ displayText }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['displayText'],
+  data: function() {
+    return {
+      text: 'test'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
