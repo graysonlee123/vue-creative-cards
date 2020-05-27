634528a32f017a922c215eae2edc447e17870db3
Grayson Gantek <graysonleegantek@gmail.com>
2020-05-27 16:56:16 -0400

    adds styles to textOutput components based on its props

--- src/components/card/TextOutput.vue
+++ src/components/card/TextOutput.vue
@@ -1,26 +1,39 @@
 <template>
   <div>
-    <p>{{ displayText }}</p>
+    <p :style="styleObject">{{ displayText }}</p>
   </div>
 </template>
 
 <script>
 export default {
-  props: ['displayText'],
-}
+  props: {
+    displayText: [String],
+    containerHeight: {
+      type: Number,
+      default: 200
+    }
+  },
+  computed: {
+    styleObject: function() {
+      return {
+        height: this.containerHeight + "px"
+      };
+    }
+  }
+};
 </script>
 
 <style scoped>
-  p {
-    font-family: 'Tangerine', cursive;
-    font-size: 42px;
-    line-height: 42px;
-    text-shadow: 2px 2px 2px #aaa;
-    color: #4d4d4d;
-    margin: 5px 0;
-    border: 1px dotted grey;
-    /* Preserves line breaks */
-    white-space: pre-line;
-    overflow: hidden;
-  }
+p {
+  font-family: "Tangerine", cursive;
+  font-size: 42px;
+  line-height: 42px;
+  text-shadow: 2px 2px 2px #aaa;
+  color: #4d4d4d;
+  margin: 5px 0;
+  border: 1px dotted grey;
+  /* Preserves line breaks */
+  white-space: pre-line;
+  overflow: hidden;
+}
 </style>
