ba92a5ef2788c7f386bf68a2de62fa5a1d95505e
Grayson Gantek <graysonleegantek@gmail.com>
2020-05-26 23:45:21 -0400

    adds a tags to nav; different bootstrap cdn; adds card front compononent

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <div class="row">
       <div class="col-sm-12">
         <nav-header></nav-header>
+        <card-front></card-front>
       </div>
     </div>
   </div>
@@ -10,14 +11,19 @@
 
 <script>
 import Header from './components/Header.vue';
+import CardFront from './components/card/CardFront.vue'
 
 export default {
   components: {
-    'nav-header': Header
+    'nav-header': Header,
+    'card-front': CardFront
   }
 }
 </script>
 
 <style>
-
+  body {
+    font-family: Verdana, Geneva, Tahoma, sans-serif;
+    color: #333; 
+  }
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,16 +3,16 @@
     <h1 class="text-center">Creative Cards</h1>
     <ul class="nav justify-content-center">
       <li class="nav-item">
-        Front
+        <a href="#" class="nav-link">Front</a>
       </li>
       <li class="nav-item">
-        Inside Left
+        <a href="#" class="nav-link">Inside Left</a>
       </li>
       <li class="nav-item">
-        Inside Right
+        <a href="#" class="nav-link">Inside Right</a>
       </li>
       <li class="nav-item">
-        Back
+        <a href="#" class="nav-link">Back</a>
       </li>
     </ul>
   </div>
