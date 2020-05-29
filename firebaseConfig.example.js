bcf91bce6564635c138991bba8b3224caf6db3c4
Grayson Gantek <graysonleegantek@gmail.com>
2020-05-28 22:25:26 -0400

    firebase example config file

---
+++ firebaseConfig.example.js
@@ -0,0 +1,15 @@
+import * as Firebase from 'firebase';
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "api-key-here",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
+};
+
+// Initialize Firebase
+Firebase.initializeApp(firebaseConfig);
