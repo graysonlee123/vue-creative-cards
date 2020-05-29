3c419cff285a79aed7ff977033d7afb6aead1190
Grayson Gantek <graysonleegantek@gmail.com>
2020-05-28 23:06:08 -0400

    adds text align options to hover form

--- src/components/card/TextOutput.vue
+++ src/components/card/TextOutput.vue
@@ -8,6 +8,21 @@
         <option value="56">56px</option>
         <option value="64">64px</option>
       </select>
+      <div class="form-check form-check-inline">
+        <label for="" class="form-check-label">
+          <input type="radio" class="form-check-input" value="left" v-model="setTextAlign"> Left
+        </label>
+      </div>
+      <div class="form-check form-check-inline">
+        <label for="" class="form-check-label">
+          <input type="radio" class="form-check-input" value="center" v-model="setTextAlign"> Center
+        </label>
+      </div>
+      <div class="form-check form-check-inline">
+        <label for="" class="form-check-label">
+          <input type="radio" class="form-check-input" value="right" v-model="setTextAlign"> Right
+        </label>
+      </div>
     </form>
     <p :style="styleObject">{{ displayText }}</p>
   </div>
@@ -25,14 +40,16 @@ export default {
   data: function() {
     return {
       showOptions: false,
-      setFontSize: ''
+      setFontSize: "",
+      setTextAlign: ""
     };
   },
   computed: {
     styleObject: function() {
       return {
         height: this.containerHeight + "px",
-        fontSize: this.setFontSize + "px"
+        fontSize: this.setFontSize + "px",
+        textAlign: this.setTextAlign
       };
     }
   }
