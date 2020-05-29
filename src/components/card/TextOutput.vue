c8c9a56c191e3f0f31cccf7fa4fc9723e3c8b6eb
Grayson Gantek <graysonleegantek@gmail.com>
2020-05-28 23:19:25 -0400

    adds italic and bold options to text ouput options

--- src/components/card/TextOutput.vue
+++ src/components/card/TextOutput.vue
@@ -2,29 +2,39 @@
   <div @mouseover="showOptions = true" @mouseleave="showOptions = false">
     <form class="small" v-show="showOptions">
       <label for="selectBox">Font size</label>
-      <select class="custom-select" id="selectBox" v-model="setFontSize">
+      <select class="custom-select custom-select-inline" id="selectBox" v-model="setFontSize">
         <option value="42">42px</option>
         <option value="48">48px</option>
         <option value="56">56px</option>
         <option value="64">64px</option>
       </select>
       <div class="form-check form-check-inline">
-        <label for="" class="form-check-label">
-          <input type="radio" class="form-check-input" value="left" v-model="setTextAlign"> Left
+        <label for class="form-check-label">
+          <input type="radio" class="form-check-input" value="left" v-model="setTextAlign" /> Left
         </label>
       </div>
       <div class="form-check form-check-inline">
-        <label for="" class="form-check-label">
-          <input type="radio" class="form-check-input" value="center" v-model="setTextAlign"> Center
+        <label for class="form-check-label">
+          <input type="radio" class="form-check-input" value="center" v-model="setTextAlign" /> Center
         </label>
       </div>
       <div class="form-check form-check-inline">
-        <label for="" class="form-check-label">
-          <input type="radio" class="form-check-input" value="right" v-model="setTextAlign"> Right
+        <label for class="form-check-label">
+          <input type="radio" class="form-check-input" value="right" v-model="setTextAlign" /> Right
+        </label>
+      </div>
+      <div class="form-check form-check-inline">
+        <label class="form-check-label">
+          <input type="checkbox" class="form-check-input" v-model="setTextBold" /> Bold
+        </label>
+      </div>
+      <div class="form-check form-check-inline">
+        <label class="form-check-label">
+          <input type="checkbox" class="form-check-input" v-model="setTextItalic" /> Italic
         </label>
       </div>
     </form>
-    <p :style="styleObject">{{ displayText }}</p>
+    <p :style="styleObject" :class="{ bold: setTextBold, italic: setTextItalic }">{{ displayText }}</p>
   </div>
 </template>
 
@@ -41,7 +51,9 @@ export default {
     return {
       showOptions: false,
       setFontSize: "",
-      setTextAlign: ""
+      setTextAlign: "",
+      setTextBold: false,
+      setTextItalic: false
     };
   },
   computed: {
@@ -68,5 +80,29 @@ p {
   /* Preserves line breaks */
   white-space: pre-line;
   overflow: hidden;
+}
+
+.bold {
+  font-weight: bold;
+}
+
+.italic {
+  font-style: italic;
+}
+
+form {
+  position: absolute;
+  border-bottom: 1px dotted grey;
+  margin-top: 10px;
+  margin-bottom: 5px;
+  padding-bottom: 5px;
+}
+
+select {
+  height: 40%;
+}
+
+.custom-select {
+  width: unset;
 }
 </style>
