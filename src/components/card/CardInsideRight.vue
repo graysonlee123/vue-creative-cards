82dc115f727e38d04a9ad11599b527791cd19921
Grayson Gantek <graysonleegantek@gmail.com>
2020-05-30 13:14:15 -0400

    inside right component

--- src/components/card/CardInsideRight.vue
+++ src/components/card/CardInsideRight.vue
@@ -1,16 +1,36 @@
 <template>
   <div class="row">
     <div class="col-sm-6 card edit-area">
-      <p>Card inside right</p>
+      <cc-text-input @displayTextChanged="textBoxValue1 = $event"></cc-text-input>
+      <cc-text-input @displayTextChanged="textBoxValue2 = $event"></cc-text-input>
+      <cc-text-input @displayTextChanged="textBoxValue3 = $event"></cc-text-input>
     </div>
-    <div class="col-sm-6 card card-display"></div>
+    <div class="col-sm-6 card card-display">
+      <cc-text-output :displayText="textBoxValue1" :containerHeight="130"></cc-text-output>
+      <cc-text-output :displayText="textBoxValue2" :containerHeight="130"></cc-text-output>
+      <cc-text-output :displayText="textBoxValue3" :containerHeight="130"></cc-text-output>
+    </div>
   </div>
 </template>
 
 <script>
+  import TextInput from './TextInput';
+  import TextOutput from './TextOutput';
 
+  export default {
+    data: function() {
+      return {
+        textBoxValue1: '',
+        textBoxValue2: '',
+        textBoxValue3: ''
+      }
+    },
+    components: {
+      ccTextInput: TextInput,
+      ccTextOutput: TextOutput
+    }
+  }
 </script>
 
 <style>
-
 </style>
